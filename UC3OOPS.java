41ee3e7f953d4b08fb3acb018556f77fd974ecb9
dhisha007 <dv6373@srmist.edu.in>
2026-03-10 08:44:08 +0530

    UC3OOPS

---
+++ UC3OOPS.java
@@ -0,0 +1,17 @@
+public class UC3OOPS {
+
+    public static void main(String[] args) {
+
+        String line1 = String.join(" ", "O", " ", "O", " ", "PPP", " ", "SSS");
+        String line2 = String.join(" ", "O", " ", "O", " ", "P", " ", "S");
+        String line3 = String.join(" ", "O", " ", "O", " ", "PPP", " ", "SS");
+        String line4 = String.join(" ", "O", " ", "O", " ", "P", " ", "S");
+        String line5 = String.join(" ", "OOO", " ", "P", " ", "SSS");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
